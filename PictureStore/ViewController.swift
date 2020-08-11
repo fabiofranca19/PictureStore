0d22387470a46cbe59beeb99a39e23f16840ba84
Fábio França <fabiotibete@gmail.com>
2020-08-11 08:40:51 -0300

    Setup Project

---
+++ PictureStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureStore
+//
+//  Created by Fábio França on 11/08/20.
+//  Copyright © 2020 music.com.chama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
